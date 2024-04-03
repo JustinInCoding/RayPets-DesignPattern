bad56006370843b78f506e5d7f6fedfa98bf4cf5
JustinSheng <justin.wang4@icloud.com>
2024-04-03 10:55:40 +0800

    Use Coordinator Part 1

--- RayPets/Coordinators/HomeCoordinator.swift
+++ RayPets/Coordinators/HomeCoordinator.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2024 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,11 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,18 +28,24 @@
 
 import UIKit
 
-@UIApplicationMain
-public class AppDelegate: UIResponder, UIApplicationDelegate {
+public class HomeCoordinator: Coordinator {
+	
+	public var children: [Coordinator] = []
+	public let router: Router
+	
+	public init(router: Router) {
+		self.router = router
+	}
+	
+	public func present(animated: Bool, onDismissed: (() -> Void)?) {
+		let viewController = HomeViewController.instantiate(delegate: self)
+		router.present(viewController, animated: animated, onDismissed: onDismissed)
+	}
+	
+}
 
-  // MARK: - Instance Properties
-	public lazy var router = AppDelegateRouter(window: window!)
-	public lazy var coordinator = HomeCoordinator(router: router)
-	public lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
-
-  // MARK: - Application Lifecycle
-  public func application(_ application: UIApplication,
-                          didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		coordinator.present(animated: true, onDismissed: nil)
-    return true
-  }
+extension HomeCoordinator: HomeViewControllerDelegate {
+	public func homeViewControllerDidPressScheduleAppointment(_ viewController: HomeViewController) {
+		
+	}
 }
